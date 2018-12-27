edf43a9adf91aac3a0d365d6b3b0d8aa123cfe48
Alan Cham <alanchamis@gmail.com>
2018-12-27 01:37:43 -0800

    Sweep clunky handlers

--- bin/init.js
+++ bin/init.js
@@ -3,39 +3,9 @@
 const expressApp = require("../app/express");
 const http = require("http");
 
-const port = normalizePort(process.env.PORT || "3000");
+const port = Number(process.env.PORT || "3000");
 const server = http.createServer(expressApp);
+const onListening = () => console.info("Listening on " + server.address().port);
 
 server.listen(port);
-server.on("error", onError);
 server.on("listening", onListening);
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-  if (isNaN(port)) return val;
-  if (port >= 0) return port;
-  return false;
-}
-
-function onError(error) {
-  if (error.syscall !== "listen") throw error;
-  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-  console.info("Listening on " + bind);
-}
