1d53a91317415eb4619a81b2c45b65551d1c73ac
Alan Cham <alanchamis@gmail.com>
2019-01-09 11:21:05 -0800

    Remove default sendFile behavior

--- services/default/app/express.js
+++ services/default/app/express.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const logger = require("morgan")("combined");
-const defaultResponse = require("../lib/sendFile");
 
 const app = express();
 app.use(logger);
-app.use(defaultResponse("index.html", { root: "./public" }));
 
 module.exports = app;
--- services/default/lib/sendFile.js
+++
@@ -1,5 +0,0 @@
-const handler = (path, options) => (_req, res, _next) => {
-  res.sendFile(path, options);
-};
-
-module.exports = handler;
