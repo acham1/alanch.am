4d10e23294e3eb00f11417e5627272443e52ffd5
Alan Cham <alanchamis@gmail.com>
2019-01-20 00:59:35 -0800

    Add App Bar

--- default/src/app/common/App.js
+++ default/src/app/common/App.js
@@ -1,9 +1,32 @@
 import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import * as colors from "@material-ui/core/colors";
 import "./App.css";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: colors.amber
+  }
+});
 
 class App extends Component {
   render() {
-    return <div className="App"> </div>;
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <AppBar color="primary" position="fixed">
+            <Toolbar>
+              <Typography variant="h6" color="inherit">
+                Title
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </div>
+      </MuiThemeProvider>
+    );
   }
 }
 
