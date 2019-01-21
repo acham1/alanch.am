732e6717523230f13ef16fdefbe695fc425bcb99
Alan Cham <alanchamis@gmail.com>
2019-01-20 23:13:59 -0800

    Create and connect redux store

--- default/src/app/common/App.js
+++ default/src/app/common/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import * as muiCore from "@material-ui/core";
 import * as muiStyles from "@material-ui/core/styles";
 import MenuIcon from "@material-ui/icons/Menu";
+import { connect } from "react-redux";
 
 const theme = muiStyles.createMuiTheme({
   palette: {
@@ -20,9 +21,15 @@ class App extends Component {
         <div className="App">
           <muiCore.AppBar color="primary" position="fixed">
             <muiCore.Toolbar variant="dense">
-              <muiCore.IconButton color="inherit" onClick={console.log}>
+              <muiCore.IconButton
+                color="inherit"
+                onClick={clickEvent => console.log(clickEvent.target)}
+              >
                 <MenuIcon />
               </muiCore.IconButton>
+              <muiCore.Typography variant="h6" color="inherit">
+                Posts
+              </muiCore.Typography>
             </muiCore.Toolbar>
           </muiCore.AppBar>
         </div>
@@ -31,4 +38,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect()(App);
--- default/src/index.js
+++ default/src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as redux from "redux";
+import * as reactRedux from "react-redux";
 import "./index.css";
 import App from "./app/common/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const reduxStore = redux.createStore((previousState, action) => previousState);
+
+ReactDOM.render(
+  <reactRedux.Provider store={reduxStore}>
+    <App />
+  </reactRedux.Provider>,
+  document.getElementById("root")
+);
