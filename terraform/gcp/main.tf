2f52b9596a3c52b5a550be10e56971c9b61e38ce
Alan Cham <alanchamis@gmail.com>
2019-06-18 12:57:04 -0700

    Enable billing for smorgasbord project

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -8,3 +8,11 @@ terraform {
 provider "google" {
   version = "~> 2.8"
 }
+
+data "google_organization" "alanch-am-organization" {
+  domain = "alanch.am"
+}
+
+data "google_billing_account" "default-billing-account" {
+  billing_account = "014B35-6D408B-620FBA"
+}
--- terraform/gcp/projects.tf
+++ terraform/gcp/projects.tf
@@ -1,9 +1,6 @@
-data "google_organization" "alanch-am-organization" {
-  domain = "alanch.am"
-}
-
 resource "google_project" "smorgasbord" {
-  name       = "smorgasbord"
-  project_id = "alanch-am-smorgasbord"
-  org_id     = data.google_organization.alanch-am-organization.id
+  name            = "smorgasbord"
+  project_id      = "alanch-am-smorgasbord"
+  org_id          = data.google_organization.alanch-am-organization.id
+  billing_account = data.google_billing_account.default-billing-account.id
 }
