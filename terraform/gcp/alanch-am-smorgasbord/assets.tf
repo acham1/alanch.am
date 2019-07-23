77398fd804624693ad8eb265fd5dbaaea2bdf50c
acham1 <alanchamis@gmail.com>
2019-07-22 21:56:57 -0700

    Specify US-WEST1 location

--- terraform/gcp/alanch-am-smorgasbord/assets.tf
+++ terraform/gcp/alanch-am-smorgasbord/assets.tf
@@ -1,6 +1,8 @@
 resource "google_storage_bucket" "static-assets" {
   name = "assets-temp.alanch.am"
 
+  location = "US-WEST1"
+
   website {
     main_page_suffix = "index.html"
     not_found_page   = "404.html"
