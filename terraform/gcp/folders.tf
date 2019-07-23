0b5247071906eedb9e1c3bd7f09ca48d9584696a
acham1 <alanchamis@gmail.com>
2019-07-22 22:32:26 -0700

    Set folders for init and smorgasbord projects

--- terraform/gcp/folders.tf
+++ terraform/gcp/folders.tf
@@ -2,3 +2,8 @@ resource "google_folder" "alanch-am-play" {
   display_name = "play"
   parent       = "organizations/1069814000115"
 }
+
+resource "google_folder" "alanch-am-init" {
+  display_name = "init"
+  parent       = "organizations/1069814000115"
+}
--- terraform/gcp/projects.tf
+++ terraform/gcp/projects.tf
@@ -4,12 +4,11 @@ resource "google_project" "smorgasbord" {
 
   billing_account = data.google_billing_account.default-billing-account.id
   folder_id       = google_folder.alanch-am-play.id
-  org_id          = data.google_organization.alanch-am-organization.id
 }
 
 resource "google_project" "init" {
   name            = "init"
   project_id      = "alanch-am-init"
-  org_id          = data.google_organization.alanch-am-organization.id
   billing_account = data.google_billing_account.default-billing-account.id
+  folder_id       = google_folder.alanch-am-init.id
 }
