e36bc548eae037ec03fab0a70a4f736f158dd917
acham1 <alanchamis@gmail.com>
2019-07-22 22:03:17 -0700

    TF import the init project

--- terraform/gcp/projects.tf
+++ terraform/gcp/projects.tf
@@ -4,3 +4,10 @@ resource "google_project" "smorgasbord" {
   org_id          = data.google_organization.alanch-am-organization.id
   billing_account = data.google_billing_account.default-billing-account.id
 }
+
+resource "google_project" "init" {
+  name            = "init"
+  project_id      = "alanch-am-init"
+  org_id          = data.google_organization.alanch-am-organization.id
+  billing_account = data.google_billing_account.default-billing-account.id
+}
