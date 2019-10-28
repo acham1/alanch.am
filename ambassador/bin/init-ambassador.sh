1612382f4d47bb6e7f3d8c3996ec29c02bc225aa
acham1 <alanchamis@gmail.com>
2019-10-27 22:33:27 -0700

    Add comments describing script

--- ambassador/bin/init-ambassador.sh
+++ ambassador/bin/init-ambassador.sh
@@ -2,5 +2,7 @@
 
 set -euxo pipefail
 
+printf "\nInstall Ambassador to Current Kubernetes Context\n"
+
 helm upgrade --install --wait ambassador stable/ambassador
  
--- helm/bin/init-helm.sh
+++ helm/bin/init-helm.sh
@@ -2,6 +2,9 @@
 
 set -euxo pipefail
 
+printf "\nInstall Tiller to Current Context\n"
+printf "\nNote: in-cluster Tiller will be removed in Helm v3\n"
+
 brew install kubernetes-helm
 
 # See: https://helm.sh/docs/using_helm/#role-based-access-control
